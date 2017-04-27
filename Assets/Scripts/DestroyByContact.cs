9928f6f097295e7219d73d867cacefebc7a684a2
plumskl0 <plumskl0@users.noreply.github.com>
2017-04-27 11:53:00 +0200

    Added extended Version

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -7,16 +7,35 @@ public class DestroyByContact : MonoBehaviour {
     public GameObject explosion;
     public GameObject playerExplosion;
 
+    public int scoreValue;
+
+    private GameController gameController;
+
+    private void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+            gameController = gameControllerObject.GetComponent<GameController>();
+
+        if (gameController == null)
+            Debug.Log("Cannot find 'GameController' script");
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Boundary"))
+        if (other.gameObject.CompareTag("Boundary") || other.gameObject.CompareTag("Enemy"))
             return;
 
-        Instantiate(explosion, transform.position, transform.rotation);
+        if (explosion != null)
+            Instantiate(explosion, transform.position, transform.rotation);
 
         if (other.gameObject.CompareTag("Player"))
+        {
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
+        }
 
+        gameController.AddScore(scoreValue);
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-    public GameObject hazard;
+    public GameObject[] hazards;
     public Vector3 spawnValues;
     public int hazardCount;
     public float spawnWait;
     public float startWait;
     public float waveWait;
 
+    public GUIText scoreText;
+    public GUIText restartText;
+    public GUIText gameOverText;
+
+    private bool gameOver;
+    private bool restart;
+
+    private int score;
+
     private void Start()
     {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        score = 0;
+        updateScore();
         StartCoroutine(SpawnWaves());
+    }
+
+    private void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
     }
 
     private IEnumerator SpawnWaves()
@@ -23,6 +49,7 @@ public class GameController : MonoBehaviour {
         { 
             for (int i = 0; i < hazardCount; i++)
             {
+                GameObject hazard = hazards[Random.Range(0,hazards.Length)];
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
 
@@ -30,7 +57,30 @@ public class GameController : MonoBehaviour {
                 yield return new WaitForSeconds(spawnWait);
             }
             yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press R 'for' Restart";
+                restart = true;
+                break;
+            }
         }
     }
 
+    public void AddScore(int newScore)
+    {
+        score += newScore;
+        updateScore();
+    }
+
+    void updateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
 }
